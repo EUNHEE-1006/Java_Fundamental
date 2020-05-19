edecb895e2b6360cb2603f90106f944f41dbb78d
EUNHEE-1006 <pmi3001@gmail.com>
2020-05-19 17:16:54 +0900

    2020-05-19-v1

--- src/Java_20200519/Calendar.java
+++ src/Java_20200519/Calendar.java
@@ -5,6 +5,13 @@ public class Calendar {
 
 	// year변수에 대해 변경할 일이 많다? =>getter, setter 함수
 	// 오직 메서드를 통해서만 데이터를 변경하게 하라!!(setter)
+	public static final int SUNDAY = 0;
+	public static final int MONDAY = 1;
+	public static final int TUESDAY = 2;
+	public static final int WEDNESDAY = 3;
+	public static final int THURSDAY = 4;
+	public static final int FRIDAY = 5;
+	public static final int SATURDAY = 6;
 
 	public Calendar() {
 		super();
@@ -32,34 +39,34 @@ public class Calendar {
 		}
 		return monthArray[month - 1];
 	}
-	
+
 	public void print(int year, int month, int day) {
 
 		int totalCount = getCount(year, month, day);
-		int rest = 0;
+		int dayOfWeek = 0;
 		String message = null;
 
-		rest = totalCount % 7;
+		dayOfWeek = totalCount % 7;
 
-		if (rest == 1) {
+		if (dayOfWeek == Calendar.MONDAY) {
 			message = "월요일";
-		} else if (rest == 2) {
+		} else if (dayOfWeek == Calendar.TUESDAY) { // 코딩할 때, 숫자를 쓰는건 그닥 좋은거 아니야.
 			message = "화요일";
-		} else if (rest == 3) {
+		} else if (dayOfWeek == Calendar.WEDNESDAY) {
 			message = "수요일";
-		} else if (rest == 4) {
+		} else if (dayOfWeek == Calendar.THURSDAY) {
 			message = "목요일";
-		} else if (rest == 5) {
+		} else if (dayOfWeek == Calendar.FRIDAY) {
 			message = "금요일";
-		} else if (rest == 6) {
+		} else if (dayOfWeek == Calendar.SATURDAY) {
 			message = "토요일";
-		} else {
+		} else if (dayOfWeek == Calendar.SUNDAY) {
 			message = "일요일";
 		}
 
 		System.out.println(year + "년 " + month + "월 " + day + "일은 " + message + " 입니다.");
 	}
-	
+
 	public void print(int year, int month) {
 		System.out.println("일\t월\t화\t수\t목\t금\t토");
 
@@ -86,8 +93,6 @@ public class Calendar {
 			print(year, i);
 		}
 	}
-
-	
 
 }
 //메서드 자동 생성 => 마우스 오른쪽 - refector
---
+++ src/Java_20200519/SuperDemo.java
@@ -0,0 +1,5 @@
+package Java_20200519;
+
+public class SuperDemo {
+
+}
