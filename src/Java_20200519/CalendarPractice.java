8136a723c9c812f53479ea017fd9745843ee75e1
EUNHEE-1006 <pmi3001@gmail.com>
2020-05-19 18:18:15 +0900

    2020-05-19-v1

--- src/Java_20200519/CalendarPractice.java
+++ src/Java_20200519/CalendarPractice.java
@@ -2,14 +2,14 @@ package Java_20200519;
 
 public class CalendarPractice {
 
-	public static final int SUNDAY=0;
-	public static final int MONDAY=1;
-	public static final int TUESDAY=2;
-	public static final int WEDNESDAY=3;
-	public static final int THURSDAY=4;
-	public static final int FRIDAY=5;
-	public static final int SATURDAY=6;
-	
+	public static final int SUNDAY = 0;
+	public static final int MONDAY = 1;
+	public static final int TUESDAY = 2;
+	public static final int WEDNESDAY = 3;
+	public static final int THURSDAY = 4;
+	public static final int FRIDAY = 5;
+	public static final int SATURDAY = 6;
+
 	public CalendarPractice() {
 		super();
 		// TODO Auto-generated constructor stub
@@ -24,43 +24,45 @@ public class CalendarPractice {
 		if (isLeafYear) {
 			monthArray[1] = 29;
 		}
-		
-		for (int i = 0; i < month-1; i++) {
-			totalCount+=monthArray[i];
+
+		for (int i = 0; i < month - 1; i++) {
+			totalCount += monthArray[i];
 		}
-		totalCount+=day;
+		totalCount += day;
 		return totalCount;
 
 	}
+
 	public void print(int year, int month, int day) {
-		int totalCount=getCount(year, month, day);
-		
-		int dayOfWeek = totalCount%7;
-		String message=null;
-		
-		
-		if(dayOfWeek==CalendarPractice.MONDAY) {
-			message="월요일";
-		}else if (dayOfWeek==CalendarPractice.TUESDAY) {
-			message="화요일";
-		}else if (dayOfWeek==CalendarPractice.WEDNESDAY) {
-			message="수요일";
-		}else if (dayOfWeek==CalendarPractice.THURSDAY) {
-			message="목요일";
-		}else if (dayOfWeek==CalendarPractice.FRIDAY) {
-			message="금요일";
-		}else if (dayOfWeek==CalendarPractice.SATURDAY) {
-			message="토요일";
-		}else if (dayOfWeek==CalendarPractice.SUNDAY) {
-			message="일요일";
+		int totalCount = getCount(year, month, day);
+
+		int dayOfWeek = totalCount % 7;
+		String message = null;
+
+		if (dayOfWeek == CalendarPractice.MONDAY) {
+			message = "월요일";
+		} else if (dayOfWeek == CalendarPractice.TUESDAY) {
+			message = "화요일";
+		} else if (dayOfWeek == CalendarPractice.WEDNESDAY) {
+			message = "수요일";
+		} else if (dayOfWeek == CalendarPractice.THURSDAY) {
+			message = "목요일";
+		} else if (dayOfWeek == CalendarPractice.FRIDAY) {
+			message = "금요일";
+		} else if (dayOfWeek == CalendarPractice.SATURDAY) {
+			message = "토요일";
+		} else if (dayOfWeek == CalendarPractice.SUNDAY) {
+			message = "일요일";
 		}
-		
+
 		System.out.println(year + "년 " + month + "월 " + day + "일은 " + message + "입니다.");
 	}
-	
+
 	public void print(int year, int month) {
 		System.out.println("일\t월\t화\t수\t목\t금\t토");
-		
+		int dayOfFirst = getCount(year, month, 0);
+
+
 	}
 
 }
